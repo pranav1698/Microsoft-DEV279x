45b1f5c8e7df0c367c3138da632298227f4e5675
Pranav Kulshrestha <hulkcodexl16@gmail.com>
2018-09-09 12:59:21 +0530

    Added code for form validation

---
+++ FormValidation/script.js
@@ -0,0 +1,27 @@
+var numberInput = document.getElementById('number');
+
+var validationMessage = document.getElementById('validationMessage');
+console.log(validationMessage);
+function verify(){
+    if(numberInput.value < 40 || numberInput.value > 90){
+        validationMessage.style.display = "block";
+        return false;
+      }
+      else if(isNaN(numberInput.value)){
+        nanMessage.style.display = "block";
+      }
+      else{
+        validationMessage.style.display = "none";
+        return true;
+      }
+    }
+    
+    numberInput.onchange = function(){
+      if(isNaN(numberInput.value)){
+        nanMessage.style.display = "block";
+      }
+      else{
+        nanMessage.style.display = "none";
+      }
+}
+
